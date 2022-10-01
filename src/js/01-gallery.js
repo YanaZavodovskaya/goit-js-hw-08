a22460b5978b2e1a398f73b58d83cb359a4841f8
YanaZavodovskaya <104728312+YanaZavodovskaya@users.noreply.github.com>
2022-10-02 01:52:32 +0600

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryWrapper = document.querySelector('.gallery');
+
+const createGallery = createGalleryItems(galleryItems);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>
+    `;
+    }).join('');
+}
+
+galleryWrapper.insertAdjacentHTML('beforeend', createGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
